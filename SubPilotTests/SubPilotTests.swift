eee1c416eb45757e8e5ae7d3a567f3ac5f400041
Charlie Nguyen <142789895+CharlieTuNguyen@users.noreply.github.com>
2025-10-13 13:53:34 +0100

    Initial Commit

---
+++ SubPilotTests/SubPilotTests.swift
@@ -0,0 +1,17 @@
+//
+//  SubPilotTests.swift
+//  SubPilotTests
+//
+//  Created by Charlie Nguyen on 13/10/2025.
+//
+
+import Testing
+@testable import SubPilot
+
+struct SubPilotTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ SubPilotUITests/SubPilotUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  SubPilotUITestsLaunchTests.swift
+//  SubPilotUITests
+//
+//  Created by Charlie Nguyen on 13/10/2025.
+//
+
+import XCTest
+
+final class SubPilotUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
